624b7641bd13fc3ce0dd2df2702ca267dcb1a34b
jromero789 <juanromerogonzalez9@gmail.com>
2022-12-05 12:14:17 +0100

    get user returns its comments

--- practica2/src/main/java/com/cloudapps/practica2/username/UsernameDTO.java
+++ practica2/src/main/java/com/cloudapps/practica2/username/UsernameDTO.java
@@ -1,7 +1,12 @@
 package com.cloudapps.practica2.username;
 
+import java.util.List;
+
+import com.cloudapps.practica2.review.Review;
+
 record UsernameDTO(
     Long id, 
     String name, 
-    String email){
+    String email, 
+    List<Review> reviews){
 }
