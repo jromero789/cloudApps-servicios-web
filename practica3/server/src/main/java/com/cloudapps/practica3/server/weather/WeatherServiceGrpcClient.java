ac62252db499cfb58f1912713696535b4be43b78
jromero789 <juanromerogonzalez9@gmail.com>
2022-12-11 04:50:45 +0100

    weather grpc client with errors

---
+++ practica3/server/src/main/java/com/cloudapps/practica3/server/weather/WeatherServiceGrpcClient.java
@@ -0,0 +1,24 @@
+package com.cloudapps.practica3.server.weather;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+
+
+@Controller
+public class WeatherServiceGrpcClient implements CommandLineRunner {
+
+	@GrpcClient("weatherServer")
+	private WeatherServiceBlockingStub client;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+        WeatherRequest request = WeatherRequest.newBuilder().setCity("Madrid").build();	        
+        WeatherResponse response = client.hello(request);
+
+	    System.out.println("Response received from server:\n" + response);
+
+		
+	}	
+}
