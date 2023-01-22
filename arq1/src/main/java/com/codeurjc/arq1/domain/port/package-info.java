00bf161fe75a988dabe7cd817695d1dad831980c
jromero789 <juanromerogonzalez9@gmail.com>
2023-01-22 08:55:57 -0600

    add more modulith allowed dependencies

---
+++ arq1/src/main/java/com/codeurjc/arq1/domain/port/package-info.java
@@ -0,0 +1,2 @@
+@org.springframework.modulith.NamedInterface("port")
+package com.codeurjc.arq1.domain.port;
---
+++ arq1/src/main/java/com/codeurjc/arq1/services/package-info.java
@@ -0,0 +1,5 @@
+@org.springframework.modulith.ApplicationModule(
+  allowedDependencies = {"dtos::response", "dtos::request", "domain", "domain::port"}
+  
+)
+package com.codeurjc.arq1.services;
